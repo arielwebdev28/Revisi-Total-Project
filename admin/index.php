fea78f942c88bfa2db733feab372cac9b506d83c
Ariel Gunawan <itsmeriell28@gmail.com>
2024-05-11 02:18:11 +0800

    nambahin yang kurang

--- admin/index.php
+++ admin/index.php
@@ -1,44 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>RPL SMKN 1 PRINGGABAYA PRODUCTION</title>
-        <!-- Favicon-->
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-        <!-- link css -->
-        <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
-    </head>
-    <body style="background-color: rgb(58, 79, 172);">
-        <!-- Content-->
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>RPL PRODUCTION - ADMIN</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../assets/img/favicon.png" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="../assets/css/main.css" rel="stylesheet">
+</head>
+
+<body class="starter-page-page">
+  <main class="main">
+    <!-- Starter Section Section -->
+    <section id="produk-section" class="starter-section section">
         <div class="container d-flex justify-content-center p-5 w-50">
-            <main class="form-signin w-75 mt-5">
-            <form action="" name="" method="">
-                <h1 class="display-5 mb-3 text-center fw-normal text-light">Sign In</h1>
-                <p class="text-light text-center">Silahkan Login untuk mengakses page Admin</p>
-                <div class="form-floating">
-                <input type="text" class="form-control" id="floatingInput" placeholder="Username" name="">
-                <label for="floatingInput">Username</label>
-                </div>
-                <div class="form-floating my-4">
-                <input type="password" class="form-control" id="floatingInput" placeholder="Password" name="">
-                <label for="floatingInput">Password</label>
-                </div>
-                <div class="my-4">
-                    <button class="btn btn-primary w-100 py-2" type="submit" name="">Login</button>
-                </div>
-            </form>
+            <main class="form-signin w-75 py-5 mt-5">
+                <form action="" name="" method="">
+                    <h1 class="display-5 mb-3 text-center fw-normal text-dark">Sign In</h1>
+                    <p class="text-dark text-center">Silahkan Login untuk mengakses page Admin</p>
+                    <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="Username" name="">
+                    <label for="floatingInput">Username</label>
+                    </div>
+                    <div class="form-floating my-4">
+                    <input type="password" class="form-control" id="floatingInput" placeholder="Password" name="">
+                    <label for="floatingInput">Password</label>
+                    </div>
+                    <div class="my-4">
+                        <button class="btn btn-primary w-100 py-2" type="submit" name="">Login</button>
+                    </div>
+                </form>
             </main>
         </div>
-        <!-- Content -->
-        
-        <!-- Footer-->
-        <div class="container">
-            <footer class="py-1 my-1 text-light">
-            <p class="text-center">&copy; 2024 <b><a href="https://github.com/arielwebdev28" target="_blank" class="text-decoration-none text-white">Ariel Gunawan</a> & <a href="https://github.com/muhamad-amin" target="_blank" class="text-decoration-none text-white">Muhamad Amin</a></b></p>
-            </footer>
-        </div>
-    </body>
+    </section>
+    <!-- /Starter Section Section -->
+  </main>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../assets/vendor/aos/aos.js"></script>
+  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="../assets/js/main.js"></script>
+
+</body>
+
 </html>
